31494ab007a3da598ac7f92cd251c07c1c77c038
Damien Rice <damien.rice123@gmail.com>
2024-07-23 15:55:07 +1000

    Adding eveent viewer to evk Code still need to add camera objects and handle collecting sensor height and width and camera frame setting within event loop

--- Code/record_raw_ekv4_w_temp_and_illum.py
+++ Code/record_raw_ekv4_w_temp_and_illum.py
@@ -4,6 +4,9 @@ import datetime
 import pathlib
 import json
 import time
+import cv2
+import asyncio
+import numpy as np
 
 import neuromorphic_drivers as nd
 
@@ -15,6 +18,35 @@ configuration = nd.prophesee_evk4.Configuration(
         diff_on=73,  # default: 73
     )
 )
+
+class Camera:
+    frame = None
+    def __init__(self, idx):
+        self._idx = idx
+
+    @property
+    def identifier(self):
+        return self._idx
+
+    # The camera class should contain a "get_frame" method
+    async def get_frame(self):
+        frameout = cv2.imencode('.jpg', self.frame)[1]
+        await asyncio.sleep(1 / 25)
+        return frameout.tobytes()
+    
+    async def set_frame(self, packet):
+        self.frame[
+            packet["dvs_events"]["x"],
+            packet["dvs_events"]["y"],
+            ] = packet["dvs_events"]["t"].astype(np.float32) * (
+            packet["dvs_events"]["on"].astype(np.float32) * 2.0 - 1.0
+            )
+        
+    def stop(self):
+        '''
+        dummy method
+        '''
+        pass
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("serial", help="Camera serial number (for example 00050423)")
@@ -81,6 +113,7 @@ with nd.open(configuration=configuration, raw=True, serial=args.serial) as devic
         next_flush = start_time + flush_interval
         next_measurement = start_time
         for status, packet in device:
+            #Set Frame method here
             events.write(packet)
             events_cursor += len(packet)
             try:
