ec7c6fa86b4f4efc0e93a3c42e92fd4b9058484a
Damien Rice <damien.rice123@gmail.com>
2024-07-23 13:42:09 +1000

    updated supervisord config to run python scripts directly

--- Code/oled_display.sh
+++ Code/oled_display.sh
@@ -1,5 +1,5 @@
 while true; do
-    # python3 /root/code/fsize_delta_disp_v2.py
+    python3 /root/code/fsize_delta_disp_v2.py
     printf 'The OLED controller crashed, wait for one second and restart\n'
     sleep 1
 done
