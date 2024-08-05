5f3e7f1acc1adaa8826edaf84f87ff8ead76acb0
Damien Rice <damien.rice123@gmail.com>
2024-08-05 15:19:07 +1000

    Resolved pi camera config issues and set appropriate values for camera config

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,1 +1,1 @@
-docker run --privileged -v /run/udev:/run/udev:ro -v /home/daedalus/daedalus_core/Data:/root/data -p 9001:9001 -p 8000:8000 -p 8001:8001 --restart always daedaluscore:latest
+docker run --privileged -v /run/udev:/run/udev:ro -v /home/daedalus/daedalus_core/Data:/root/data -p 9000:9000 -p 8000:8000 -p 8001:8001 --restart always daedaluscore:latest
