1d96d0d844d531e723843f8342e5f4fe213a1b8e
Damien Rice <damien.rice123@gmail.com>
2024-08-26 15:07:08 +1000

    added sudo back because of errors

--- daedalusInstall.sh
+++ daedalusInstall.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 sudo usermod -aG root $USER
-raspi-config nonint do_spi 0
-raspi-config nonint do_i2c 0
-raspi-config nonint do_serial_hw 0
-raspi-config nonint do_serial_cons 0
-raspi-config nonint do_onewire 0
-raspi-config nonint do_rgpio 0
+sudo raspi-config nonint do_spi 0
+sudo raspi-config nonint do_i2c 0
+sudo raspi-config nonint do_serial_hw 0
+sudo raspi-config nonint do_serial_cons 0
+sudo raspi-config nonint do_onewire 0
+sudo raspi-config nonint do_rgpio 0
 
-sed -i 's/dtparam=i2c_arm=on/dtparam=i2c_arm=on,i2c_arm_baudrate=40000/g' /boot/firmware/config.txt
-echo "usb_max_current_enable=1" >> /boot/firmware/config.txt
+sudo sed -i 's/dtparam=i2c_arm=on/dtparam=i2c_arm=on,i2c_arm_baudrate=40000/g' /boot/firmware/config.txt
+sudo echo "usb_max_current_enable=1" >> /boot/firmware/config.txt
 
-sed -i 's/#HandlePowerKey=poweroff/HandlePowerKey=ignore/g' /etc/systemd/logind.conf
+sudo sed -i 's/#HandlePowerKey=poweroff/HandlePowerKey=ignore/g' /etc/systemd/logind.conf
 
-echo "RuntimeWatchdogSec=15" >> /etc/systemd/system.conf
-echo "RebootWatchdogSec=2min" >> /etc/systemd/system.conf
+sudo echo "RuntimeWatchdogSec=15" >> /etc/systemd/system.conf
+sudo echo "RebootWatchdogSec=2min" >> /etc/systemd/system.conf
 
 mkdir $HOME/data
 cp -a Code $HOME/code
 cp -a Config $HOME/config
 
-apt-get update
+sudo apt-get update
 
-add-apt-repository 'ppa:deadsnakes/ppa'
+sudo add-apt-repository 'ppa:deadsnakes/ppa'
 
-apt-get install -y \
+sudo apt-get install -y \
     build-essential \
     curl \
     supervisor \
@@ -36,7 +36,7 @@ apt-get install -y \
     python3-pip
 
 
-apt install -y python3-picamera2 --no-install-recommends
+sudo apt install -y python3-picamera2 --no-install-recommends
 
 curl https://sh.rustup.rs -sSf | bash -s -- -y
 
@@ -50,4 +50,4 @@ cp $HOME/config/65-neuromorphic-drivers.rules /etc/udev/rules.d/65-neuromorphic-
 cp $HOME/config/99-camera.rules /etc/udev/rules.d/99-camera.rules
 cp $HOME/config/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 
-systemctl daemon-reload
+sudo systemctl daemon-reload
