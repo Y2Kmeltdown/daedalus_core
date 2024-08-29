dcea1030c41058f9ab2a07f7de1c5759dc79a833
Damien Rice <damien.rice123@gmail.com>
2024-08-29 15:31:22 +1000

    should hopefully work

--- daedalusInstall.sh
+++ daedalusInstall.sh
@@ -43,12 +43,22 @@ pip install --break-system-packages -r /usr/local/config/requirements.txt
 sudo cp /usr/local/config/65-neuromorphic-drivers.rules /etc/udev/rules.d/65-neuromorphic-drivers.rules
 sudo cp /usr/local/config/99-camera.rules /etc/udev/rules.d/99-camera.rules
 
-sudo mkdir /etc/supervisor/conf.d
+sudo mkdir -p /etc/supervisor/conf.d
 sudo cp /usr/local/config/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 
 sudo apt-get install -y \
     supervisor
 
+echo 'Daedalus Core Installed successfully to view running processes visit http://daedalus.local or enter the command supervisorctl status\nReconfiguring eth0 to host device and rebooting.\nPlease Wait.'
+sleep 10
+# nmcli con delete DAEDALUS_ETH
+# nmcli con add type ethernet ifname eth0 con-name DAEDALUS_ETH
+# nmcli con modify DAEDALUS_ETH ipv4.method shared ipv4.address 192.168.5.1/24
+# nmcli con modify DAEDALUS_ETH ipv6.method disabled
+# nmcli con modify DAEDALUS_ETH connection.autoconnect yes
+# nmcli con up DAEDALUS_ETH
+
+# sudo reboot
 #su $SUDO_USER -c '/usr/bin/supervisord -c /etc/supervisor/supervisord.conf'
 
 
