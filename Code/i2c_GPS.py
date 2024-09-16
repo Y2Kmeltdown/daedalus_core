0ff1f146dc8d494e2537a362515bc0c05abb16af
Y2Kmeltdown <damien.rice123@gmail.com>
2024-09-17 09:29:43 +1000

    i2c code with GPS data parsing options

--- Code/i2c_GPS.py
+++ Code/i2c_GPS.py
@@ -1,5 +1,4 @@
 from smbus2 import SMBus
-import time
 
 i2cbus = SMBus(1)
 
@@ -9,34 +8,74 @@ AvBytes_REG1 = 0xFD
 AvBytes_REG2 = 0xFE
 DataStream_REG = 0xFF
 
-count = 0
-GPS_Packets = []
-while(True):
-    AvBytes1 = i2cbus.read_byte_data(i2cAddress, AvBytes_REG1)
-    AvBytes2 = i2cbus.read_byte_data(i2cAddress, AvBytes_REG2)
+def get_gps_burst() -> list[bytearray]:
+    GPS_Packet = []
+    packet = bytearray()
+    while(True):
+        AvBytes1 = i2cbus.read_byte_data(i2cAddress, AvBytes_REG1)
+        AvBytes2 = i2cbus.read_byte_data(i2cAddress, AvBytes_REG2)
 
-    if AvBytes1 != 0 or AvBytes2 != 0:
-        DataStream = i2cbus.read_byte_data(i2cAddress, DataStream_REG)
-
-        #with open("test.txt", "ab") as file:
-            #file.write(DataStream.to_bytes(1, 'big'))
-        
-        if DataStream == 36:
-            packet = bytearray()
-            packet.append(DataStream)
-        elif DataStream == 10:
-            GPS_Packets.append(packet)
-            packet = bytearray
-            count+=1
-        elif DataStream == 13:
-            pass
-        else:
-            packet.append(DataStream)
-        
-        if len(GPS_Packets) >= 50:
-            break
-
-print(GPS_Packets)
+        if AvBytes1 != 0 or AvBytes2 != 0:
+            DataStream = i2cbus.read_byte_data(i2cAddress, DataStream_REG)
             
+            if DataStream == 36: # If value in data stream is $ ascii character
+                packet = bytearray()
+                packet.append(DataStream)
+            elif DataStream == 10: # If value in data stream is newline character "\n"
+                GPS_Packet.append(packet)
+                packet = bytearray()
+            elif DataStream == 13: # If value in data stream is carriage return character "\r"
+                pass
+            else:
+                packet.append(DataStream)
+            
+        elif GPS_Packet != []:
+            break
+    return GPS_Packet
+
+def get_rmc(gps_burst: list[bytearray]):
+    packerHeader = []
+    rmc = [i.decode("utf-8").split(",") for i in gps_burst if i.find(b'$GNRMC') != -1][0]
+    print(rmc)
+
+def get_vtg(gps_burst: list[bytearray]):
+    packetHeader = []
+    vtg = [i.decode("utf-8").split(",") for i in gps_burst if i.find(b'$GNVTG') != -1][0]
+    print(vtg)
+
+def get_gga(gps_burst: list[bytearray]):
+    packetHeader = []
+    gga = [i.decode("utf-8").split(",") for i in gps_burst if i.find(b'$GNGGA') != -1][0]
+    print(gga)
+
+def get_gll(gps_burst: list[bytearray]):
+    packetHeader = []
+    gll = [i.decode("utf-8").split(",") for i in gps_burst if i.find(b'$GNGLL') != -1][0]
+    print(gll)
+
+def get_gsa(gps_burst: list[bytearray]):
+    packetHeader = []
+    gsa = [i.decode("utf-8").split(",") for i in gps_burst if i.find(b'$GNGSA') != -1]
+
+def get_gsv(gps_burst: list[bytearray]):
+    packetHeader = []
+    gpgsv = [i.decode("utf-8").split(",") for i in gps_burst if i.find(b'$GPGSV') != -1]
+    glgsv = [i.decode("utf-8").split(",") for i in gps_burst if i.find(b'$GLGSV') != -1]
+    gagsv = [i.decode("utf-8").split(",") for i in gps_burst if i.find(b'$GAGSV') != -1]
+    gbgsv = [i.decode("utf-8").split(",") for i in gps_burst if i.find(b'$GBGSV') != -1]
+    gqgsv = [i.decode("utf-8").split(",") for i in gps_burst if i.find(b'$GQGSV') != -1]
+    
+
+# packetHeader = [headers]
+# byte.decode("utf-8")
+# string.split(",")
+# 
+
+if __name__ == "__main__":
+    while(True):
+        GPS_burst = get_gps_burst()
+        get_rmc(GPS_burst)
+        get_vtg(GPS_burst)
+        get_gga(GPS_burst)
 
     
