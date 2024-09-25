a13cdd44d48ba0cf424f10916fcefd90089d4dba
Y2Kmeltdown <damien.rice123@gmail.com>
2024-09-25 16:35:30 +1000

    Reducing time to 10 seconds

--- Code/WatchDog.py
+++ Code/WatchDog.py
@@ -3,11 +3,11 @@ import smbus2
 import time
 
 addr = 0x67
-WATCH_ON_OFF      = 0x01    #开关看门狗功能
-WATCH_TIME        = 0x02    #设置超时时间
-WATCH_REMAIN_TIME = 0x03    #返回剩余时间
-WATCH_STATE       = 0x04    #LED多功能
-WATCH_FwVersion   = 0x05    #软件版本
+WATCH_ON_OFF      = 0x01
+WATCH_TIME        = 0x02
+WATCH_REMAIN_TIME = 0x03
+WATCH_STATE       = 0x04
+WATCH_FwVersion   = 0x05
 
 WATCH_ON  = 0x03
 WATCH_OFF = 0x02
@@ -20,7 +20,7 @@ WATCH_OFF_LED = 0x00
 
 WATCH_version = 0x01
 
-WATCH_TIME_Restart = 60 
+WATCH_TIME_Restart = 10
 
 Feed_dogs = 4
 try:
@@ -47,19 +47,18 @@ try:
         bus.write_i2c_block_data(addr,address,temp)
 
 
-    if read(WATCH_FwVersion) == WATCH_version: #Access version number 访问版本号
+    if read(WATCH_FwVersion) == WATCH_version: #Access version number
         print("init succeed")
-        write(WATCH_ON_OFF,WATCH_ON)#Enable watchdog function 开看门狗功能
+        write(WATCH_ON_OFF,WATCH_ON)#Enable watchdog function
         time.sleep(0.5)
         write(WATCH_STATE,WATCH_ON_LED | WATCH_NO_Timeout)  #Turn on the LED status indicator and clear the timeout flag
-                                                            # 开LED状态指示灯 并清除超时标志
-        write_word(WATCH_TIME,WATCH_TIME_Restart) #Set the timeout, currently 60 seconds 设置超时时间，当前时间为60秒
+        write_word(WATCH_TIME,WATCH_TIME_Restart) #Set the timeout, currently 60 seconds
 
     else:
         print("init fail")
 
     while True:
-        #Feed the dog every 0.8 seconds  每0.8秒喂一次狗
+        #Feed the dog every 0.8 seconds 0.8
         Feed_dogs.on()
         time.sleep(0.8)
         Feed_dogs.off()
