365ffe7292a65acbb1428c76372ce8ac82032ed8
Y2Kmeltdown <damien.rice123@gmail.com>
2024-09-27 20:51:23 +1000

    Comments to make code easier to follow

--- Code/gps_resolver.py
+++ Code/gps_resolver.py
@@ -19,24 +19,31 @@ def packetRepairer(packet:bytes, repairLimit:int=1) -> tuple[bytes, bool, bool] 
     valid = False
     repaired = False
     errors = []
+
+    # Check for how many error bits are in packet
     for index, byte in enumerate(packet[1:-4]):
         if byte == 127:
             errors.append(index)
-    if len(errors) == 0:
-        if checksumValidator:
+
+    if len(errors) == 0: # If no error bits are detected do a checksum validation
+        if checksumValidator: # If checksum is good return original packet and stats
             outPacket = packet
             valid = True
             repaired = False
-        else:
+        else: # if checksum is bad return original packet and fail
             outPacket = packet
             valid = False
             repaired = False
-    if len(errors) <= repairLimit:
-        testSolution = [44]*len(errors)
-        #Underneath Repair Limit Attempt repairing
 
-        for i in range(pow(256,len(errors))):
-            #Test here
+    if len(errors) <= repairLimit: # If number of errors is lower than set repair limit attempt to repair the packet
+        testSolution = [44]*len(errors)
+        bottomAscii = 44 # ","
+        topAscii = 90 # "Z"
+        asciiRange = topAscii-bottomAscii
+
+        for i in range(pow(asciiRange,len(errors))): # Repair is O^n complexity due to needing to search for a solution through all available values.
+
+            #Test replacing error bits with test bits then check validation
             testPacket = packet
             for errorIndex, errorFix in zip(errors,testSolution):
                 testPacket = testPacket.replace(b'\x7F', errorFix.to_bytes(1,'big'), 1)
@@ -46,24 +53,26 @@ def packetRepairer(packet:bytes, repairLimit:int=1) -> tuple[bytes, bool, bool] 
                 valid = True
                 repaired = True
                 return outPacket, valid, repaired
-            #Increment here
-            for index, j in enumerate(testSolution):
+            
+            #Increment through ascii bits combinations
+            for index, testChar in enumerate(testSolution):
                 if index == 0:
-                    if j == 90:
-                        testSolution[index] = 44
+                    if testChar == topAscii:
+                        testSolution[index] = bottomAscii
                     else:
                         testSolution[index] +=1
                 else:
-                    if testSolution[index-1] == 90:
-                        if j == 90:
-                            testSolution[index] = 44
+                    if testSolution[index-1] == topAscii:
+                        if testChar == topAscii:
+                            testSolution[index] = bottomAscii
                         else:
                             testSolution[index] +=1
+        # If no solutions are found return original packet and fail
         valid = False
         repaired = False
         outPacket = packet
               
-    else:
+    else: # If number of errors are above repair limit give up life is hopeless
         valid = False
         repaired = False
         outPacket = packet
