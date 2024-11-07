7f88c61705e81cc6d3df7303211bd53cef538c15
Y2Kmeltdown <damien.rice123@gmail.com>
2024-11-07 12:28:48 +1100

    Testing With AP active immediately and change serial number for evk

--- daedalusInstall.sh
+++ daedalusInstall.sh
@@ -99,6 +99,22 @@ nmcli con modify DAEDALUS_ETH ipv6.method disabled
 nmcli con modify DAEDALUS_ETH connection.autoconnect yes
 nmcli con up DAEDALUS_ETH
 
+## WIFI AP set up
+nmcli con delete DAEDALUS-AP
+nmcli con add type wifi ifname wlan0 mode ap con-name DAEDALUS-AP ssid DAEDALUS autoconnect false
+nmcli con modify DAEDALUS-AP wifi.band bg
+nmcli con modify DAEDALUS-AP wifi.channel 3
+nmcli con modify DAEDALUS-AP wifi.cloned-mac-address 00:12:34:56:78:9a
+nmcli con modify DAEDALUS-AP wifi-sec.key-mgmt wpa-psk
+nmcli con modify DAEDALUS-AP wifi-sec.proto rsn
+nmcli con modify DAEDALUS-AP wifi-sec.group ccmp
+nmcli con modify DAEDALUS-AP wifi-sec.pairwise ccmp
+nmcli con modify DAEDALUS-AP wifi-sec.psk "daedalus"
+nmcli con modify DAEDALUS-AP ipv4.method shared ipv4.address 192.168.4.1/24
+nmcli con modify DAEDALUS-AP ipv6.method disabled
+nmcli con modify DAEDALUS-AP connection.autoconnect yes
+nmcli con up DAEDALUS-AP
+
 sudo chmod -R 777 $DAEDALUS_DIR
 
 sudo reboot
