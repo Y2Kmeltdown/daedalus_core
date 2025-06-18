1fe2d1df8347b4cab8de17c817c749125f9a0e2b
Y2Kmeltdown <damien.rice123@gmail.com>
2025-06-19 09:33:53 +1000

    Minor change to IP address on ethernet chared mode

--- daedalusInstall.sh
+++ daedalusInstall.sh
@@ -125,7 +125,7 @@ echo -e "Daedalus Core Installed successfully to view running processes visit ht
 sleep 10
 nmcli con delete DAEDALUS_ETH
 nmcli con add type ethernet ifname eth0 con-name DAEDALUS_ETH
-nmcli con modify DAEDALUS_ETH ipv4.method shared ipv4.address 192.168.5.1/24
+nmcli con modify DAEDALUS_ETH ipv4.method shared ipv4.address 169.254.100.1/16
 nmcli con modify DAEDALUS_ETH ipv6.method disabled
 nmcli con modify DAEDALUS_ETH connection.autoconnect yes
 nmcli con up DAEDALUS_ETH
